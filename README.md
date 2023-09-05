387ab818a2a7de045e59028239dc611b9764477f
= <=>
2023-09-05 17:10:34 -0300

    update instructions

--- README.md
+++ README.md
@@ -8,11 +8,12 @@
 You are creating the backend for a new client who wants a website to manage their hotels.
 
 It needs to:
+
 1. Allow finding a particular hotel
-2. Update hotel details
-3. Add a new hotel (name, address)
-4. Delete a hotel
+2. Add a new hotel (name, address)
+3. Delete a hotel
+4. Update hotel details
 
-Please get those routes working and then if you like you can improve things.
+Please get those routes working and then if you like, you can improve things.
 
-Feel free to ask questions!  Have fun!
+Feel free to ask questions! Have fun!
--- index.ts
+++ index.ts
@@ -1,16 +1,20 @@
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
-import { database } from './db';
+import express, { Express, Request, Response } from "express";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import { database } from "./db";
 
 dotenv.config();
 
 const app: Express = express();
+app.use(bodyParser.json({ limit: "100mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+
 const port = process.env.PORT;
 
-app.get('/', (req: Request, res: Response) => {
-    res.json(database);
+app.get("/", (req: Request, res: Response) => {
+  res.json(database);
 });
 
 app.listen(port, () => {
-    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
